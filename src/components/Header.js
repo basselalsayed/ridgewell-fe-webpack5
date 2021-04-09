3c66aab3d9399e3ddd5d9c42183a897c9b405b2d
basselalsayed <alsayed.bassel@gmail.com>
2021-04-09 20:02:26 +0100

    was not using rr...

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,44 @@ import { useAuth } from 'hooks/useAuth';
 import { isAdmin } from 'helpers';
 
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const { user, logOut } = useAuth();
   const showAdminBoard = user && isAdmin(user);
 
-  const adminNavigation = <Nav.Link href="/admin">Admin Board</Nav.Link>;
+  const adminNavigation = (
+    <Nav.Link as={Link} to="/admin">
+      Admin Board
+    </Nav.Link>
+  );
 
   const leftNavigation = user && (
     <>
       {showAdminBoard && adminNavigation}
-      <Nav.Link href="/user">User</Nav.Link>
+      <Nav.Link as={Link} to="/user">
+        User
+      </Nav.Link>
     </>
   );
 
   const auth = user && (
     <>
-      <Nav.Link href="/profile">{user.username}</Nav.Link>
+      <Nav.Link as={Link} to="/profile">
+        {user.username}
+      </Nav.Link>
       <Nav.Link onClick={logOut}>Log Out</Nav.Link>
     </>
   );
 
   const noAuth = (
     <>
-      <Nav.Link href="/login">Login</Nav.Link>
-      <Nav.Link href="/register">Sign Up</Nav.Link>
+      <Nav.Link as={Link} to="/login">
+        Login
+      </Nav.Link>
+      <Nav.Link as={Link} to="/register">
+        Sign Up
+      </Nav.Link>
     </>
   );
 
@@ -34,7 +47,9 @@ const Header = () => {
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">Ridgewell House</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        Ridgewell House
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">{leftNavigation}</Nav>
