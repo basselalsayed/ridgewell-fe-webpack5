b5a08c7cc1adf0dae5691a29cf0ca7bc8f53cad5
basselalsayed <alsayed.bassel@gmail.com>
2021-04-18 17:10:23 +0100

    using new BoardDisplay component in requests

--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,33 +1,17 @@
 import { useRequests } from 'hooks/redux/useRequests';
-import { Spinner } from 'react-bootstrap';
-import { Request } from './request';
+
+import UniversalComponent from 'components/UniversalComponent';
 
 const Requests = () => {
   const { requests, loading } = useRequests();
-
-  return loading ? (
-    <Spinner
-      style={{
-        position: 'absolute',
-        color: 'green',
-        left: '50%',
-        top: '50%',
-        marginLeft: '-1rem',
-        marginTop: '1rem',
-      }}
-      animation="border"
+  return (
+    <UniversalComponent
+      export="BoardDisplay"
+      content={requests}
+      componentExport="Request"
+      loading={loading}
+      emptyMessage="No requests awaiting approval"
     />
-  ) : (
-    <div
-      style={{
-        maxHeight: window.innerHeight - 150,
-        overflow: 'auto',
-      }}
-    >
-      {requests.length === 0
-        ? 'No requests awaiting approval'
-        : requests.map((req) => <Request key={req.id} {...req} />)}
-    </div>
   );
 };
 
