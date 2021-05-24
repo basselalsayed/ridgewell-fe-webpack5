48124d0bc4f2e409ec476fb861dce726f2b3a218
basselalsayed <alsayed.bassel@gmail.com>
2021-05-24 17:04:18 +0100

    return isAdmin memoized value

--- src/hooks/redux/useAuth.js
+++ src/hooks/redux/useAuth.js
@@ -1,12 +1,15 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { login, signUp, logOut } from 'store/actions/auth';
 
 const useAuth = () => {
   const dispatch = useDispatch();
+
   const { loggedIn, user } = useSelector((state) => state.auth, shallowEqual);
+
   const history = useHistory();
+
   const _login = useCallback(async (userInfo) => dispatch(login(userInfo)), [
     dispatch,
   ]);
@@ -19,9 +22,14 @@ const useAuth = () => {
     dispatch,
   ]);
 
+  const isAdmin = useMemo(() => loggedIn && user.roles.includes('ROLE_ADMIN'), [
+    user,
+  ]);
+
   // const loggedIn = useMemo(() => !!user, [user]);
 
   return {
+    isAdmin,
     login: _login,
     logOut: _logOut,
     loggedIn,
