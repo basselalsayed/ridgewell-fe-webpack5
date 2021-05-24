b651fdda689b7ca77061351b4a1ce2483f96f4d9
basselalsayed <alsayed.bassel@gmail.com>
2021-05-24 16:44:19 +0100

    use new pattern and wrap in memo

--- src/containers/BoardUser.js
+++ src/containers/BoardUser.js
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { Tab, Row, Col, ListGroup, Spinner } from 'react-bootstrap';
+import { memo, useEffect } from 'react';
+import { Tab, Row, Col, ListGroup } from 'react-bootstrap';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { getAll } from 'store/modules';
 import { setError } from 'store/modules/response';
@@ -7,8 +7,7 @@ import { Holidays, Notifications, Requests } from 'components';
 import { useAuth } from 'hooks';
 import { tabBtn } from 'components/index.module.css';
 
-const BoardUser = () => {
-  const { error } = useSelector((state) => state.response, shallowEqual);
+const BoardUser = memo(() => {
   const { loggedIn, user } = useAuth();
   const dispatch = useDispatch();
 
@@ -21,11 +20,7 @@ const BoardUser = () => {
     getData();
   }, [dispatch, loggedIn, user.id]);
 
-  const {
-    holidays: { holidays },
-    notifications,
-    requests,
-  } = useSelector((state) => state.content, shallowEqual);
+  const { notifications } = useSelector((state) => state.content, shallowEqual);
 
   //  <div className='container'>
   // <header className='jumbotron'>
@@ -33,24 +28,17 @@ const BoardUser = () => {
     <Row>
       <Col>
         <ListGroup horizontal>
-          <ListGroup.Item
-            action
-            className={tabBtn}
-            href="#requests"
-            children="Requests"
-          />
-          <ListGroup.Item
-            action
-            className={tabBtn}
-            href="#holidays"
-            children="Holidays"
-          />
-          <ListGroup.Item
-            action
-            className={tabBtn}
-            href="#notifications"
-            children="Notifications"
-          />
+          <ListGroup.Item action className={tabBtn} href="#requests">
+            Requests
+          </ListGroup.Item>
+
+          <ListGroup.Item action className={tabBtn} href="#holidays">
+            Holidays
+          </ListGroup.Item>
+
+          <ListGroup.Item action className={tabBtn} href="#notifications">
+            Notifications
+          </ListGroup.Item>
         </ListGroup>
       </Col>
     </Row>
@@ -59,31 +47,17 @@ const BoardUser = () => {
   const tabContent = (
     <Row>
       <Col>
-        {error ? null : holidays && requests && notifications ? (
-          <Tab.Content>
-            <Tab.Pane eventKey="#requests">
-              <Requests requests={requests} />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#holidays">
-              <Holidays holidays={holidays} />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#notifications">
-              <Notifications notifications={notifications} />
-            </Tab.Pane>
-          </Tab.Content>
-        ) : (
-          <Spinner
-            style={{
-              position: 'inherit',
-              color: 'green',
-              left: '50%',
-              top: '50%',
-              marginLeft: '-1rem',
-              marginTop: '1rem',
-            }}
-            animation="border"
-          />
-        )}
+        <Tab.Content>
+          <Tab.Pane eventKey="#requests">
+            <Requests />
+          </Tab.Pane>
+          <Tab.Pane eventKey="#holidays">
+            <Holidays />
+          </Tab.Pane>
+          <Tab.Pane eventKey="#notifications">
+            <Notifications notifications={notifications} />
+          </Tab.Pane>
+        </Tab.Content>
       </Col>
     </Row>
   );
@@ -97,6 +71,6 @@ const BoardUser = () => {
     //   {/* </header> */}
     // {/* </div> */}
   );
-};
+});
 
 export { BoardUser };
