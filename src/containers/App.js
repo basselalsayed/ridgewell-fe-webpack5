4c58f45ef0d113984f55bf3d4ae1d42759960dea
basselalsayed <alsayed.bassel@gmail.com>
2021-05-24 17:03:22 +0100

    use default bootstrap css

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './bootstrap.min.css';
 import './App.css';
 
 import { Header, Alert } from 'components';
