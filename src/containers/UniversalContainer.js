1a59af45b3555b7118a6169c2751fad720e17e1a
basselalsayed <alsayed.bassel@gmail.com>
2021-05-24 17:30:12 +0100

    UniversalContainer fix

--- src/containers/UniversalContainer.js
+++ src/containers/UniversalContainer.js
@@ -1,7 +1,14 @@
 import loadable from '@loadable/component';
 
-export default loadable((props) =>
-  import(`./${props.name}`).then(
-    (module) => module.default || module[props.name]
-  )
+export default loadable(
+  (props) => import(`${`./${props.page}` || 'containers'}`),
+  {
+    resolveComponent: (mod, props) => mod[props.page],
+  }
 );
+
+// const old = loadable((props) =>
+//   import(`./${props.name}`).then(
+//     (module) => module.default || module[props.name]
+//   )
+// );
