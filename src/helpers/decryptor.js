1e53594b251df06d589f4fc89221a1c0915115bb
basselalsayed <alsayed.bassel@gmail.com>
2021-05-25 15:49:10 +0100

    clg keys

--- src/helpers/decryptor.js
+++ src/helpers/decryptor.js
@@ -10,6 +10,8 @@ const [algorithm, hex, iv, key, utf8] = [
   'utf-8',
 ];
 
+console.log('iv, key', iv, key);
+
 const decrypt = (encrypted) => {
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
 
