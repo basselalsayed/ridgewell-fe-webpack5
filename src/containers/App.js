df55ee3794318411ea9d9ada760e2d9f9af8f03f
basselalsayed <alsayed.bassel@gmail.com>
2021-05-25 11:35:32 +0100

    clgs for heroku

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,6 +17,11 @@ const App = ({ route }) => {
     }
   }, []);
 
+  useEffect(() => {
+    console.log(process.env);
+    console.log('process.env.API_URL', process.env.API_URL);
+  }, []);
+
   useNetwork();
 
   return (
