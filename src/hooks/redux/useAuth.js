63a1b711176ca88b602dca70765efbd1b29ca892
basselalsayed <alsayed.bassel@gmail.com>
2021-06-02 01:32:09 +0100

    various changes

--- src/hooks/redux/useAuth.js
+++ src/hooks/redux/useAuth.js
@@ -1,25 +1,49 @@
 import { isAdmin as isAdminHelper } from 'helpers';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { login, signUp, logOut } from 'store/actions/auth';
+import { useHistory, useLocation } from 'react-router';
+
+import {
+  login as _login,
+  signUp as _signUp,
+  logOut as _logOut,
+  getOwnHolidays as _getOwnHolidays,
+} from 'store/modules';
+import { useHolidays } from './useHolidays';
 
 const useAuth = () => {
   const dispatch = useDispatch();
 
-  const { loggedIn, user } = useSelector((state) => state.auth, shallowEqual);
+  const { user, loadingHolidays, loadedHolidays, holidays } = useSelector(
+    ({ auth }) => auth,
+    shallowEqual
+  );
+
+  const loggedIn = useMemo(() => !!user, [user]);
 
   const history = useHistory();
+  const { pathname } = useLocation();
+  // const { getUserHolidays } = useHolidays();
 
-  const _login = useCallback(async (userInfo) => dispatch(login(userInfo)), [
+  const login = useCallback(async (userInfo) => dispatch(_login(userInfo)), [
     dispatch,
   ]);
 
-  const _signUp = useCallback((userInfo) => dispatch(signUp(userInfo)), [
+  const isOnUserPage = useMemo(() => /user/.test(pathname), [pathname]);
+
+  const getOwnHolidays = useCallback(() => dispatch(_getOwnHolidays(user.id)));
+
+  useEffect(() => {
+    if (loggedIn && isOnUserPage && !loadingHolidays && !loadedHolidays) {
+      getOwnHolidays();
+    }
+  }, [loggedIn, loadingHolidays, loadedHolidays, isOnUserPage]);
+
+  const signUp = useCallback((userInfo) => dispatch(_signUp(userInfo)), [
     dispatch,
   ]);
 
-  const _logOut = useCallback(() => (dispatch(logOut()), history.push('/')), [
+  const logOut = useCallback(() => (dispatch(_logOut()), history.push('/')), [
     dispatch,
   ]);
 
@@ -28,14 +52,16 @@ const useAuth = () => {
     user,
   ]);
 
-  // const loggedIn = useMemo(() => !!user, [user]);
-
   return {
+    holidays,
     isAdmin,
-    login: _login,
-    logOut: _logOut,
+    isOnUserPage,
+    loadingHolidays,
+    loadedHolidays,
+    login,
+    logOut,
     loggedIn,
-    signUp: _signUp,
+    signUp,
     user,
   };
 };
