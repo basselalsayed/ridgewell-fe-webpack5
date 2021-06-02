bed04d84491c160bfabd9103d0bb9fd4b479f523
basselalsayed <alsayed.bassel@gmail.com>
2021-06-02 01:26:56 +0100

    scss change

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './bootstrap.min.css';
-import './App.css';
+import './App.scss';
 
 import { renderRoutes } from 'react-router-config';
 import { useNetwork } from 'hooks/useNetwork';
