a11b0b67880a3bed2bce307bf9314fa1547c614c
basselalsayed <alsayed.bassel@gmail.com>
2021-06-02 02:32:25 +0100

    default props

--- src/components/holidays/holidays.js
+++ src/components/holidays/holidays.js
@@ -2,19 +2,21 @@ import UniversalComponent from 'components/UniversalComponent';
 import { useHolidays } from 'hooks';
 import { memo } from 'react';
 
-const Holidays = memo(() => {
-  const { holidays, loading, loaded } = useHolidays();
-
-  return (
+const Holidays = memo(
+  ({
+    allHolidayEntities = useHolidays().allHolidayEntities,
+    loading = useHolidays().loading,
+    loaded = useHolidays().loaded,
+  }) => (
     <UniversalComponent
       export="BoardDisplay"
-      content={holidays}
+      content={allHolidayEntities}
       componentExport="Holiday"
       loading={loading}
       loaded={loaded}
       emptyMessage="No holidays here"
     />
-  );
-});
+  )
+);
 
 export { Holidays };
