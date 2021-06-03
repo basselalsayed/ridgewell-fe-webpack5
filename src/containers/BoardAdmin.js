526baba17557008bf46cb30d3ee59afaf85e3cd1
basselalsayed <alsayed.bassel@gmail.com>
2021-06-03 03:19:37 +0100

    autoEffect for redirects with no hash value, using location hash and history push for conditional content

--- src/containers/BoardAdmin.js
+++ src/containers/BoardAdmin.js
@@ -3,23 +3,58 @@ import { Holidays, Requests, Users } from 'components';
 
 import './Board.scss';
 import 'components/skeletons/skeleton.scss';
+import { useHistory, useLocation } from 'react-router';
+import { useAutoEffect, useHolidays, useRequests } from 'hooks';
 
 const BoardAdmin = () => {
   //  <div className='container'>
   // <header className='jumbotron'>
+
+  const history = useHistory();
+  const { hash } = useLocation();
+
+  useAutoEffect({
+    condition: hash === '',
+    callback: () => history.push('#requests'),
+    deps: [],
+  });
+
+  const {
+    allHolidayEntities,
+    loading: holidaysLoading,
+    loaded: holidaysLoaded,
+  } = useHolidays();
+  const {
+    allRequestEntities,
+    loaded: requestsLoaded,
+    loading: requestsLoading,
+  } = useRequests();
+
   const tabButtons = (
     <Row>
       <Col>
         <ListGroup horizontal>
-          <ListGroup.Item action className="tabBtn" href="#requests">
+          <ListGroup.Item
+            action
+            active={hash === '#requests'}
+            onClick={() => history.push('#requests')}
+          >
             Requests
           </ListGroup.Item>
 
-          <ListGroup.Item action className="tabBtn" href="#users">
+          <ListGroup.Item
+            action
+            active={hash === '#users'}
+            onClick={() => history.push('#users')}
+          >
             Users
           </ListGroup.Item>
 
-          <ListGroup.Item action className="tabBtn" href="#holidays">
+          <ListGroup.Item
+            action
+            active={hash === '#holidays'}
+            onClick={() => history.push('#holidays')}
+          >
             Holidays
           </ListGroup.Item>
         </ListGroup>
@@ -31,15 +66,23 @@ const BoardAdmin = () => {
     <Row>
       <Col>
         <Tab.Content>
-          <Tab.Pane eventKey="#requests">
-            <Requests />
-          </Tab.Pane>
-          <Tab.Pane eventKey="#users">
-            <Users />
-          </Tab.Pane>
-          <Tab.Pane eventKey="#holidays">
-            <Holidays />
-          </Tab.Pane>
+          {hash === '#requests' && (
+            <Requests
+              requests={allRequestEntities}
+              loading={requestsLoading}
+              loaded={requestsLoaded}
+            />
+          )}
+
+          {hash === '#users' && <Users />}
+
+          {hash === '#holidays' && (
+            <Holidays
+              holidays={allHolidayEntities}
+              loading={holidaysLoading}
+              loaded={holidaysLoaded}
+            />
+          )}
         </Tab.Content>
       </Col>
     </Row>
