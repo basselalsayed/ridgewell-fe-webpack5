387555d1be21d02ef51dd1d6612a5cc4a865a8d6
basselalsayed <alsayed.bassel@gmail.com>
2021-06-03 03:13:52 +0100

    autoEffect for redirects with no hash value, userRequestEntities correctly being fetched

--- src/containers/BoardUser.js
+++ src/containers/BoardUser.js
@@ -1,63 +1,94 @@
-import { memo, useEffect, useMemo } from 'react';
+import { memo, useMemo } from 'react';
 import { Tab, Row, Col, ListGroup } from 'react-bootstrap';
 import { shallowEqual, useSelector } from 'react-redux';
 
 import { Holidays, Notifications, Requests } from 'components';
-import { useAuth, useHolidays } from 'hooks';
+import { useAuth, useAutoEffect, useHolidays, useRequests } from 'hooks';
 
 import { useHistory, useLocation } from 'react-router';
 import './Board.scss';
 import 'components/skeletons/skeleton.scss';
 
 const BoardUser = memo(() => {
-  const { holidays, loadingHolidays, loadedHolidays } = useAuth();
+  const {
+    holidays,
+    loadingHolidays,
+    loadedHolidays,
+    requests,
+    loadingRequests,
+    loadedRequests,
+  } = useAuth();
 
   const history = useHistory();
   const { hash } = useLocation();
 
+  useAutoEffect({
+    condition: hash === '',
+    callback: () => history.push('#requests'),
+    deps: [],
+  });
+
   const { getHolidayEntities } = useHolidays();
+  const { getRequestEntities } = useRequests();
   const { notifications } = useSelector((state) => state.content, shallowEqual);
 
-  const ownHolidayEntities = useMemo(() => getHolidayEntities(holidays), [
-    holidays,
-  ]);
-  //  <div className='container'>
-  // <header className='jumbotron'>
-  const tabButtons = (
-    <Row style={{ justifyContent: 'center' }}>
-      <ListGroup horizontal className="tabBtnContainer">
-        <div
-          className={`tabBtn ${hash === '#requests' ? 'active' : ''}`}
-          onClick={() => history.push('#requests')}
-        >
-          Requests
-        </div>
-
-        <div
-          className={`tabBtn ${hash === '#holidays' ? 'active' : ''}`}
-          onClick={() => history.push('#holidays')}
-        >
-          Holidays
-        </div>
-
-        <div
-          className={`tabBtn ${hash === '#notifications' ? 'active' : ''}`}
-          onClick={() => history.push('#notifications')}
-        >
-          Notifications
-        </div>
-      </ListGroup>
-    </Row>
+  const ownHolidayEntities = useMemo(
+    () => (loadedHolidays && holidays && getHolidayEntities(holidays)) || [],
+    [loadedHolidays, holidays]
+  );
+  const ownRequestEntities = useMemo(
+    () => (loadedRequests && requests && getRequestEntities(requests)) || [],
+    [loadedRequests, requests]
   );
 
+  //  <div className='container'>
+  // <header className='jumbotron'>
+
+  const tabButtons = (
+    <Row>
+      <Col>
+        <ListGroup horizontal>
+          <ListGroup.Item
+            action
+            active={hash === '#requests'}
+            onClick={() => history.push('#requests')}
+          >
+            Requests
+          </ListGroup.Item>
+
+          <ListGroup.Item
+            action
+            active={hash === '#holidays'}
+            onClick={() => history.push('#holidays')}
+          >
+            Holidays
+          </ListGroup.Item>
+
+          <ListGroup.Item
+            action
+            active={hash === '#notifications'}
+            onClick={() => history.push('#notifications')}
+          >
+            Notifications
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+    </Row>
+  );
   const tabContent = (
     <Row>
       <Col>
         <Tab.Content>
-          {hash === '#requests' && <Requests />}
+          {hash === '#requests' && (
+            <Requests
+              requests={ownRequestEntities}
+              loading={loadingRequests}
+              loaded={loadedRequests}
+            />
+          )}
           {hash === '#holidays' && (
             <Holidays
-              allHolidayEntities={ownHolidayEntities}
+              holidays={ownHolidayEntities}
               loading={loadingHolidays}
               loaded={loadedHolidays}
             />
