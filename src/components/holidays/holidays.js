53b0f839082695526e8351af4589f2aee7789669
basselalsayed <alsayed.bassel@gmail.com>
2021-06-03 01:16:44 +0100

    taking props

--- src/components/holidays/holidays.js
+++ src/components/holidays/holidays.js
@@ -1,22 +1,16 @@
 import UniversalComponent from 'components/UniversalComponent';
-import { useHolidays } from 'hooks';
+
 import { memo } from 'react';
 
-const Holidays = memo(
-  ({
-    allHolidayEntities = useHolidays().allHolidayEntities,
-    loading = useHolidays().loading,
-    loaded = useHolidays().loaded,
-  }) => (
-    <UniversalComponent
-      export="BoardDisplay"
-      content={allHolidayEntities}
-      componentExport="Holiday"
-      loading={loading}
-      loaded={loaded}
-      emptyMessage="No holidays here"
-    />
-  )
-);
+const Holidays = memo(({ holidays, loading, loaded }) => (
+  <UniversalComponent
+    export="BoardDisplay"
+    content={holidays}
+    componentExport="Holiday"
+    loading={loading}
+    loaded={loaded}
+    emptyMessage="No holidays here"
+  />
+));
 
 export { Holidays };
--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,21 +1,15 @@
-import { useRequests } from 'hooks';
-
 import UniversalComponent from 'components/UniversalComponent';
 import { memo } from 'react';
 
-const Requests = memo(() => {
-  const { requests, loaded, loading } = useRequests();
-
-  return (
-    <UniversalComponent
-      export="BoardDisplay"
-      content={requests}
-      componentExport="Request"
-      loading={loading}
-      loaded={loaded}
-      emptyMessage="No requests awaiting approval"
-    />
-  );
-});
+const Requests = memo(({ requests, loaded, loading }) => (
+  <UniversalComponent
+    export="BoardDisplay"
+    content={requests}
+    componentExport="Request"
+    loading={loading}
+    loaded={loaded}
+    emptyMessage="No requests awaiting approval"
+  />
+));
 
 export { Requests };
