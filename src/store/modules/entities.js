98386238f1799f7aa9fda5ec42fd87a9776cec31
basselalsayed <alsayed.bassel@gmail.com>
2021-06-07 02:31:55 +0100

    update notification entity logic + action type

--- src/store/modules/entities.js
+++ src/store/modules/entities.js
@@ -2,6 +2,7 @@ import produce from 'immer';
 import { merge, isEqual } from 'lodash';
 
 export const SET_ENTITIES = 'ENTITIES/SET_ENTITIES';
+export const UPDATE_NOTIFICATION_ENTITY = 'ENTITIES/UPDATE_NOTIFICATION_ENTITY';
 
 const initialState = {
   holidayRequests: {},
@@ -14,6 +15,11 @@ const entitiesReducer = produce((state, { type, payload }) => {
     case SET_ENTITIES:
       if (!isEqual(state, payload)) return merge({}, state, payload);
       return state;
+    case UPDATE_NOTIFICATION_ENTITY:
+      state.notifications[payload.id] = {
+        ...state.notifications[payload.id],
+        ...payload.data,
+      };
     // no default
   }
 }, initialState);
