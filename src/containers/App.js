26aaddfe4ae9270c7a6ca12e96bd993983e26a01
basselalsayed <alsayed.bassel@gmail.com>
2021-07-11 18:33:12 +0100

    update to import from public path '/static'

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,7 @@ const App = ({ route }) => {
     if ('serviceWorker' in navigator) {
       // Use the window load event to keep the page load performant
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./service-worker.js');
+        navigator.serviceWorker.register('./static/service-worker.js');
       });
     }
   }, []);
