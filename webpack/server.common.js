ca2ecbd9a8ec8806ff8cb014263d2856aba3bbe1
basselalsayed <alsayed.bassel@gmail.com>
2021-07-11 16:21:15 +0100

    server webpack configs

---
+++ webpack/server.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const paths = require('./paths');
+
+const { server: loaders } = require('./loaders');
+const { server: plugins } = require('./plugins');
+
+module.exports = {
+  entry: paths.server,
+  mode: 'production',
+  target: 'node',
+  node: {
+    global: false,
+    __filename: true,
+    __dirname: true,
+  },
+  output: {
+    path: paths.buildServer,
+    filename: 'server.js',
+    // chunkFilename: '[name].bundle.js',
+    publicPath: path.join(paths.buildClient, paths.public),
+  },
+  plugins,
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    fallback: {
+      stream: 'stream-browserify',
+    },
+    alias: {
+      components: path.join(paths.src, 'components'),
+    },
+  },
+  module: {
+    rules: loaders,
+  },
+};
