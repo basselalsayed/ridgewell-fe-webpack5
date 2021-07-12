f8142b55eff63a27c77555945c07c9eaed9ee2da
basselalsayed <alsayed.bassel@gmail.com>
2021-07-12 15:20:18 +0100

    convet to es5

--- src/server/coldStart.js
+++ src/server/coldStart.js
@@ -1,13 +1,12 @@
-import 'dotenv/config';
-import { logMessage } from '../../scripts/utils';
+const { logMessage } = require('../../scripts/utils');
 
 const axios = require('axios');
 
-export default async (_, __, next) => {
+module.exports = async (_, __, next) => {
   try {
     await axios.get(`${process.env.API_URL}ping`);
   } catch (error) {
-    logMessage(error, 'error');
+    logMessage(error.message, 'error');
   }
   next();
 };
