26c04093e8c3f7710d63f07d9777dfbf1742c379
basselalsayed <alsayed.bassel@gmail.com>
2021-07-12 15:21:21 +0100

    try removing babel

--- src/client/server.js
+++ src/client/server.js
@@ -1,5 +1,27 @@
-require('@babel/register')({
-  extensions: ['.js', '.jsx'],
+require('dotenv').config();
+const { join } = require('path');
+const express = require('express');
+
+const compression = require('compression');
+const paths = require('../../webpack/paths');
+const coldStart = require('../server/coldStart');
+
+const app = express();
+app.use(compression());
+app.use(coldStart);
+
+app.use(express.static(join(paths.buildClient)));
+
+app.get('/*', (_, res) => {
+  res.sendFile(join(paths.buildClient, paths.public, 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
-module.exports = require('./serverBase').default;
+app.set('port', process.env.PORT || 8080);
+
+const server = app.listen(app.get('port'), () => {
+  console.log('listening on port ', server.address().port);
+});
